d5390be385743aae7e21a46d8ddb62b823986d54
Katie Gaines <klgaines18@gmail.com>
2023-02-18 15:11:09 -0500

    update products seed script

--- src/db/seeds/01-products.js
+++ src/db/seeds/01-products.js
@@ -2,12 +2,11 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> } 
  */
+
+const products = require("../fixtures/products");
+
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
+  await knex.raw("TRUNCATE TABLE products RESTART IDENTITY CASCADE")
+  await knex('products').insert(products);
 };
