3e95b04601e83c816cd99e4c4753496d00aab991
Katie Gaines <klgaines18@gmail.com>
2023-02-18 15:12:42 -0500

    update categories seed script

--- src/db/seeds/02-categories.js
+++ src/db/seeds/02-categories.js
@@ -2,12 +2,11 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> } 
  */
+
+const categories = require("../fixtures/categories");
+
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
+  await knex.raw("TRUNCATE TABLE categories RESTART IDENTITY CASCADE")
+  await knex('categories').insert(categories);
 };
