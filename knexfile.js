092a33ad15e3e64d163d997df689569150c61385
Katie Gaines <klgaines18@gmail.com>
2023-02-18 14:24:55 -0500

    set up connection file

--- knexfile.js
+++ knexfile.js
@@ -3,11 +3,15 @@
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+
+require("dotenv").config();
+const { DATABASE_URL } = process.env;
+
 module.exports = {
 
   development: {
     client: 'postgresql',
-    connection: '',
+    connection: DATABASE_URL,
   },
 
 };
