d6107901bf80136c65cbe186923c75d2153aa586
Katie Gaines <klgaines18@gmail.com>
2023-02-19 05:24:36 -0500

    add read to products service

--- src/products/products.service.js
+++ src/products/products.service.js
@@ -4,6 +4,10 @@ function list() {
   return knex("products").select("*");
 }
 
+function read(productId) {
+  return knex("products").select("*").where({ product_id: productId }).first();
+}
+
 module.exports = {
   list,
 };
