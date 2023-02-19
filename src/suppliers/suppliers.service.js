d4389484513d0de826136d5a0c554939b90c40c1
Katie Gaines <klgaines18@gmail.com>
2023-02-19 06:09:27 -0500

    add .then updateRecords

--- src/suppliers/suppliers.service.js
+++ src/suppliers/suppliers.service.js
@@ -15,7 +15,8 @@ function update(updatedSupplier) {
   return knex("suppliers")
     .select("*")
     .where({ supplier_id: updatedSupplier.supplier_id })
-    .update(updatedSupplier, "*");
+    .update(updatedSupplier, "*")
+    .then((updatedRecords) => updatedRecords[0]);
 }
 
 module.exports = {
