61cef3c8554a2415be809f150d09d7c090223171
Katie Gaines <klgaines18@gmail.com>
2023-02-19 07:48:31 -0500

    add out-of-stock route

--- src/products/products.router.js
+++ src/products/products.router.js
@@ -2,7 +2,19 @@ const router = require("express").Router({ mergeParams: true });
 const controller = require("./products.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").get(controller.list).all(methodNotAllowed);
-router.route("/:productId([0-9]+)").get(controller.read).all(methodNotAllowed);
+router
+  .route("/")
+  .get(controller.list)
+  .all(methodNotAllowed);
+
+router
+  .route("/:productId([0-9]+)")
+  .get(controller.read)
+  .all(methodNotAllowed);
+
+router
+  .route("/out-of-stock-count")
+  .get(controller.listOutOfStockCount)
+  .all(methodNotAllowed)
 
 module.exports = router;
