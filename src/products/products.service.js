02e6d027a9b2e1feddae72af9654e4b0d0302d19
Katie Gaines <klgaines18@gmail.com>
2023-02-19 07:42:12 -0500

    add listOutOfStockCount function

--- src/products/products.service.js
+++ src/products/products.service.js
@@ -5,10 +5,22 @@ function list() {
 }
 
 function read(productId) {
-  return knex("products").select("*").where({ product_id: productId }).first();
+  return knex("products")
+    .select("*")
+    .where({ product_id: productId })
+    .first();
+}
+
+function listOutOfStockCount() {
+  return knex("products")
+    .select("product_quantity_in_stock as out_of_stock")
+    .count("product_id")
+    .where({ product_quantity_in_stock: 0 })
+    .groupBy("out_of_stock");
 }
 
 module.exports = {
   list,
   read,
+  listOutOfStockCount,
 };
