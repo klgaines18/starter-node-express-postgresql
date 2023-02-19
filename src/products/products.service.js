784b2eb5b26e1245165a7f8407104ae4591380ad
Katie Gaines <klgaines18@gmail.com>
2023-02-19 08:50:43 -0500

    modify read to join tables

--- src/products/products.service.js
+++ src/products/products.service.js
@@ -4,10 +4,12 @@ function list() {
   return knex("products").select("*");
 }
 
-function read(productId) {
-  return knex("products")
-    .select("*")
-    .where({ product_id: productId })
+function read(product_id) {
+  return knex("products as p")
+    .join("products_categories as pc", "p.product_id", "pc.product_id")
+    .join("categories as c", "pc.category_id", "c.category_id")
+    .select("p.*", "c.*")
+    .where({ "p.product_id": product_id })
     .first();
 }
 
