5696692f3addc72b47793cac2bfaaf59358349e5
Katie Gaines <klgaines18@gmail.com>
2023-02-19 06:08:55 -0500

    add read and update functions

--- src/suppliers/suppliers.service.js
+++ src/suppliers/suppliers.service.js
@@ -7,6 +7,19 @@ function create(supplier) {
     .then((createdRecords) => createdRecords[0]);
 }
 
+function read(supplier_id) {
+  return knex("suppliers").select("*").where({ supplier_id }).first();
+}
+
+function update(updatedSupplier) {
+  return knex("suppliers")
+    .select("*")
+    .where({ supplier_id: updatedSupplier.supplier_id })
+    .update(updatedSupplier, "*");
+}
+
 module.exports = {
   create,
+  read,
+  update
 };
